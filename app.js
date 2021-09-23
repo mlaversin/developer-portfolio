3dc2281fb43b4b745a562fbd769c3a0574908509
Morgan Laversin <morganlaversin@gmail.com>
2021-09-23 14:55:22 +0200

    home section

--- app.js
+++ app.js
@@ -19,3 +19,46 @@ if (window.matchMedia('(max-width: 1300px)')) {
   });
 }
 
+// Typewriter effect on Home Section
+
+const txtAnim = document.querySelector('.txt-animation');
+
+let typewriter = new Typewriter(txtAnim, {
+  loop: true,
+  deleteSpeed: 20,
+});
+
+typewriter
+  .pauseFor(1300)
+  .changeDelay(20)
+  .typeString("Moi c'est Morgan Laversin")
+  .pauseFor(300)
+  .typeString('<strong>, Développeur Full-Stack</strong> !')
+  .pauseFor(1000)
+  .deleteChars(13)
+  .typeString('<span style="color: #777bb3;"> PhP</span> !')
+  .pauseFor(1000)
+  .deleteChars(6)
+  .typeString('<span style="color: #bf47b5;"> Symfony</span> !')
+  .pauseFor(1000)
+  .deleteChars(10)
+  .typeString('<span style="color: #c48428;"> JavaScript</span> !')
+  .pauseFor(1000)
+  .deleteChars(13)
+  .typeString('<span style="color: #27ae60;"> VueJS</span> !')
+  .pauseFor(1000)
+  .deleteChars(8)
+  .typeString('<strong> Full-Stack</strong> !')
+  .pauseFor(1000)
+  .deleteChars(13)
+  .typeString('<span style="color: #777bb3;"> PhP</span> !')
+  .pauseFor(1000)
+  .deleteChars(6)
+  .typeString('<span style="color: #bf47b5;"> Symfony</span> !')
+  .pauseFor(1000)
+  .deleteChars(10)
+  .typeString('<span style="color: #c48428;"> JavaScript</span> !')
+  .pauseFor(1000)
+  .deleteChars(13)
+  .typeString('<span style="color: #27ae60;"> VueJS</span> !')
+  .start();
